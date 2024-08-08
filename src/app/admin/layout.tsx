7d4c97d34dd0d5b81f3b7cf63eb50a2d4c6b847a
drissbejgana <bejganadriss@gmail.com>
2024-08-08 11:47:28 +0100

    done

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
 export const metadata = {
     title: 'Admin Page',
@@ -11,9 +12,14 @@ export const metadata = {
     children: React.ReactNode;
   }) {
     const session = await getServerSession(options);
+    if(!session){
+      redirect("/api/auth/signin?callbackUrl=/"); 
+    }
+   else{
+      if(session.user.role!="Admin")
+        return <h1 className="text-center text-5xl">No access</h1>
+   }
 
-    if(session.user.role!="Admin")
-      return <h1 className="text-center text-5xl">No access</h1>
 
     
     return (
