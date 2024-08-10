540987fbf0aea08737b02af2bb5ca4668f32322c
drissbejgana <93830096+drissbejgana@users.noreply.github.com>
2024-08-10 16:09:57 +0100

    Update Salles.tsx

--- src/app/components/Salles.tsx
+++ src/app/components/Salles.tsx
@@ -38,7 +38,7 @@ useEffect(()=>{
     <>
               
          <h1 className="text-center text-4xl my-5 ">Salles</h1>
-             <div className="grid gap-4 mb-5 md:mb-0 grid-cols-1 md:grid-cols-3 place-content-center place-items-center">   { !laoding? 
+             <div className="grid gap-4 mb-5 md:mb-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-content-center place-items-center">   { !laoding? 
               
                 salles?.map(item=>
                        <button  onClick={()=>router.push(`/salles/${item._id}`)} key={item._id}  className={`cuve w-[350px] h-[350px] shadow-md transform transition duration-500 ease-in-out bg-blue-300 cursor-pointer hover:scale-105 hover:bg-blue-500 hover:text-white`}>
