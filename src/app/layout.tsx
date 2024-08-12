28c2fdd04bfc12d2be69cdc2cafdc99049e8a49f
drissbejgana <bejganadriss@gmail.com>
2024-08-12 12:00:37 +0100

    some modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import dynamic from "next/dynamic";
 import { Navbar } from "./components/Navbar";
 import AuthProvider from "./components/AuthProvider";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
 
 
@@ -19,11 +22,17 @@ export const metadata: Metadata = {
   description: "Gestion de maintenance ",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession(options);
+  if(!session){
+    redirect("/api/auth/signin?callbackUrl=/"); 
+  }
+
   return (
     <html lang="en">
        <AuthProvider>
