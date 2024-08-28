5ec7ed962e86a7a2f4fd9716be8b8bf05be25a0c
drissbejgana <bejganadriss@gmail.com>
2024-08-28 11:49:52 +0100

    text

--- src/app/components/AddRapportToEquipment.tsx
+++ src/app/components/AddRapportToEquipment.tsx
@@ -91,7 +91,7 @@ const AddRapportToEquipment = ({equipment}: {equipment:Equipment}) => {
            </div>
 
             <div className="mb-5">
-               <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rapport Name: </label>
+               <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Problème signalé : </label>
                <input type="text" onChange={(e)=>setRapport({...rapport,name:e.currentTarget.value})}  id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
             </div>
 
--- src/app/components/Rapports.tsx
+++ src/app/components/Rapports.tsx
@@ -89,7 +89,7 @@ useEffect(()=>{
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                          Name
+                             Problème signalé
                         </th>
                         <th scope="col" className="px-6 py-3">
                             Maintenance
